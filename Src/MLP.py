1d84dd0ebe8807f3bda22c81fa7f580eb92c2b80
Philip Bollen <trik-flip@users.noreply.github.com>
2020-12-15 19:46:04 +0100

    Add docstring and auto format

--- Src/MLP.py
+++ Src/MLP.py
@@ -2,36 +2,33 @@ import numpy as np
 
 
 class MLP:
-
+    """A simple Multi layer network implementation"""
     def __init__(self, num_input=3, num_hidden=[3, 5], num_output=2) -> None:
+        """Create a MLP"""
         self.num_input = num_input
         self.num_hidden = num_hidden
         self.num_output = num_output
-
         layers = [self.num_input] + self.num_hidden + [self.num_output]
-
         self.weigths = []
         for i in range(len(layers)-1):
             w = np.random.rand(layers[i], layers[i+1])
             self.weigths.append(w)
 
     def forward_propagate(self, activations):
+        """AKA Predict"""
         for w in self.weigths:
             net_inputs = np.dot(activations, w)
-
             activations = self._sigmoid(net_inputs)
         return activations
 
     def _sigmoid(self, x):
+        """Return Sigmoid function of x"""
         return 1 / (1 + np.exp(-x))
+
 
 if __name__ == "__main__":
     mlp = MLP()
-
     inputs = np.random.rand(mlp.num_input)
-
-
     ouputs = mlp.forward_propagate(inputs)
-
     print(f"input:{inputs}")
     print(f"output:{ouputs}")
