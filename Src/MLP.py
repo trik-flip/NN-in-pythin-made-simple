95a9f66b395fdf4b3b74ad43c6585b9feee610c7
Philip Bollen <trik-flip@users.noreply.github.com>
2020-12-15 19:43:27 +0100

    Add forward propagation

--- Src/MLP.py
+++ Src/MLP.py
@@ -14,3 +14,24 @@ class MLP:
         for i in range(len(layers)-1):
             w = np.random.rand(layers[i], layers[i+1])
             self.weigths.append(w)
+
+    def forward_propagate(self, activations):
+        for w in self.weigths:
+            net_inputs = np.dot(activations, w)
+
+            activations = self._sigmoid(net_inputs)
+        return activations
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+if __name__ == "__main__":
+    mlp = MLP()
+
+    inputs = np.random.rand(mlp.num_input)
+
+
+    ouputs = mlp.forward_propagate(inputs)
+
+    print(f"input:{inputs}")
+    print(f"output:{ouputs}")
